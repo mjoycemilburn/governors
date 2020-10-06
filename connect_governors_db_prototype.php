7fa742407a6f5c3f13144fd8c19b39412aa80279
MartinJ <mjoycemilburn@gmail.com>
2020-10-06 12:33:59 +0100

    Create  connect_governors_db_prototype.php

---
+++ connect_governors_db_prototype.php
@@ -0,0 +1,9 @@
+<?php
+
+# This routine need to be  placed in the home directory of your server. 
+# 
+
+$con = mysqli_connect('hostname','root','password','database_name'); //**CONFIG REQUIRED**
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
